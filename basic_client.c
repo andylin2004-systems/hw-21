db6de5caaf6bc8020bf0a6bd989d8aa07f108fbd
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-14 13:39:47 -0500

    ctrl+c event handling done

--- basic_client.c
+++ basic_client.c
@@ -2,9 +2,14 @@
 
 
 int main() {
-
   int to_server;
   int from_server;
 
   from_server = client_handshake( &to_server );
+
+  while(1){
+    char input[BUFFER_SIZE] = {0};
+    read(STDIN_FILENO, input, sizeof(input));
+    write(to_server, input, sizeof(input));
+  }
 }
--- basic_server.c
+++ basic_server.c
@@ -2,9 +2,18 @@
 
 
 int main() {
-
   int to_client;
   int from_client;
 
-  from_client = server_handshake( &to_client );
+  while(1){
+    from_client = server_handshake( &to_client );
+
+    while(1){
+      char input[BUFFER_SIZE] = {0};
+      if (read(from_client, input, sizeof(input)) == 0){
+        break;
+      }
+      printf("%s", input);
+    }
+  }
 }
